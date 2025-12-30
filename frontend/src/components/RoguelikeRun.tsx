7a559df5df4414314c71051b731bca47fdd397de
AdelineMeisterFormatrice <meistertzheimadeline@gmail.com>
2025-12-30 19:39:06 +0100

    ajout mode roguelike

--- frontend/src/components/RoguelikeRun.tsx
+++ frontend/src/components/RoguelikeRun.tsx
@@ -10,28 +10,6 @@ import { applyPerk } from "./applyPerk";
 import { generatePerkChoices } from "../utils/perkRng";
 import { ALL_PERKS } from "../data/perks";
 
-const MOCK_PERKS: Perk[] = [
-  {
-    id: 'extra-hold',
-    name: '+1 HOLD',
-    description: 'Stockez une pièce supplémentaire',
-    rarity: 'common',
-  },
-  {
-    id: 'slow-gravity',
-    name: 'GRAVITÉ LENTE',
-    description: 'La vitesse de chute est réduite',
-    rarity: 'rare',
-  },
-  {
-    id: 'bomb',
-    name: 'BOMBE',
-    description: 'Détruit une zone 3x3',
-    rarity: 'epic',
-  },
-]
-
-
 export default function RoguelikeRun() {
     const [selectingPerk, setSelectingPerk] = useState(true)
     const [activePerks, setActivePerks] = useState<Perk[]>([]);
